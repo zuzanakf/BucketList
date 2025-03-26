5a914e8c3d05915f5165a26aec2070be242823b4
Zuzana Fernandes <zuzana.fernandes@yahoo.co.uk>
2025-03-26 18:05:25 +0000

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Zuzana.Fernandes on 26/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketListTests/BucketListTests.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListTests.swift
+//  BucketListTests
+//
+//  Created by Zuzana.Fernandes on 26/03/2025.
+//
+
+import Testing
+@testable import BucketList
+
+struct BucketListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
