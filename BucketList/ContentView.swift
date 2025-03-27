377d5ab44a44df776ce59d64a0b279ed7871d4f6
Zuzana Fernandes <zuzana.fernandes@yahoo.co.uk>
2025-03-27 15:14:09 +0000

    adding conformance to Comparable (custom types), and saving data to documents directory

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
+// user struct
+struct User: Comparable, Identifiable {
+    let id = UUID()
+    var firstName: String
+    var lastName: String
+    
+    static func <(lhs: User, rhs: User) -> Bool{
+        lhs.lastName < rhs.lastName
+    }
+}
+
 struct ContentView: View {
+    var users = [
+        User(firstName: "Zuzana", lastName: "Kubascikova"),
+        User(firstName: "Jana", lastName: "Kriz"),
+        User(firstName: "Petr", lastName: "Mojtek"),
+        User(firstName: "Vojtech", lastName: "Svoboda"),
+    ].sorted()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(users) { user in
+            Text("\(user.firstName) \(user.lastName)")
         }
-        .padding()
+        //saving data to documents
+        Button("read and write") {
+            let data = Data("test message".utf8)
+            let url = URL.documentsDirectory.appending(path: "message.txt")
+            
+            do {
+                try data.write(to: url, options: [.atomic, .completeFileProtection])
+                let input = try String(contentsOf: url)
+                print(input)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
