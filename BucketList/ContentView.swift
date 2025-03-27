76eec4889cd8799f7883f3bbd6082a8e5fba42c1
Zuzana Fernandes <zuzana.fernandes@yahoo.co.uk>
2025-03-27 15:21:46 +0000

    switching view state w/ enums

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 // user struct
+// using Comparable on cutsom types
 struct User: Comparable, Identifiable {
     let id = UUID()
     var firstName: String
@@ -18,17 +19,56 @@ struct User: Comparable, Identifiable {
     }
 }
 
+
+// swicthing view states with enums
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading...")
+    }
+}
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailureView: View {
+    var body: some View {
+        Text("Failed!")
+    }
+}
+
 struct ContentView: View {
+    // using Comparable on cutsom types
     var users = [
         User(firstName: "Zuzana", lastName: "Kubascikova"),
         User(firstName: "Jana", lastName: "Kriz"),
         User(firstName: "Petr", lastName: "Mojtek"),
         User(firstName: "Vojtech", lastName: "Svoboda"),
     ].sorted()
+    
+    // swicthing view states with enums
+    enum LoadingState {
+        case loading, sucess, failed
+    }
+    @State private var loadingState = LoadingState.loading
+    
     var body: some View {
+        // using Comparable on cutsom types
         List(users) { user in
             Text("\(user.firstName) \(user.lastName)")
         }
+        
+        // swicthing view states with enums
+        if loadingState == .loading {
+            LoadingView()
+        } else if loadingState == .sucess {
+            SuccessView()
+        } else {
+            FailureView()
+        }
+        
+        
         //saving data to documents
         Button("read and write") {
             let data = Data("test message".utf8)
